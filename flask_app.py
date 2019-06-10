cdcc0eb106420543165ce8e05a155f2d70750597
Aibaki <48203047+Aibaki@users.noreply.github.com>
2019-06-10 10:56:05 +0200

    First Commit

---
+++ flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import subprocess
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/create_user", methods=['POST'])
+def create_user():
+    """
+        Method to run a server-side script to create a user using payload information
+
+        :returns Response from Server Script Call
+    """
+    credentials = request.get_json(request.data)
+    username = credentials['credentials']['username']
+    password = credentials['credentials']['password']
+
+    # Primary key in db; incremental
+    uid = 1001
+    # gid=0 for root gid=100 for normal users
+    gid = 100
+    path = credentials['sftp_details']['path']
+
+    # The above variables form part of the arguments used by the script to create a user
+    arguments = username + ':' + password + ':' + str(uid) + ":" + str(gid) + ':' + path
+
+    # Calling the server-side script in this format: create-sftp-user username:Password:uid:gid:upload-folder
+    ret = subprocess.call("create-sftp-user.py " + arguments, shell=True)
+    return str(ret)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
